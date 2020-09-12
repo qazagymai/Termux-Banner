ab863346addb61af19a6c403c49cfd94634f530d
qazagymai <71091073+qazagymai@users.noreply.github.com>
2020-09-12 23:37:41 +0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-clear && echo "Installing..." && pkg update -y && pkg upgrade -y && pkg install figlet -y && pkg install python -y && pip install lolcat && clear && echo "Successfully installed" && chmod +x setbanner.sh && ./setbanner.sh
+clear && echo "Installing..." && pkg update -y && pkg upgrade -y && pkg install figlet -y && pkg install python2 -y && pip2 install lolcat && clear && echo "Successfully installed" && chmod +x setbanner.sh && ./setbanner.sh
